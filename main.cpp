33fe62c1a2526057cbe2bada64dbeb0f14836763
ratcker <ratckeryt@gmail.com>
2026-07-02 14:25:13 +0300

    added main functions

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ std::vector<FileMeta> scan_directory(const std::string& target) {
                 meta.size = fs::file_size(entry.path());
                 auto last_mod_time = fs::last_write_time(entry.path());
                 auto epoch = last_mod_time.time_since_epoch();
-                meta.modification_time = std::chrono::duration_cast<chrono::seconds>(epoch).count();
+                meta.modification_time = std::chrono::duration_cast<std::chrono::seconds>(epoch).count();
                 checked.push_back(meta);//и заносим их в вектор всех метаданных файлов
             }
         }
@@ -54,8 +54,86 @@ void save_snapshot(const std::string& db_path, const std::vector<FileMeta>& file
     std::cout << "snapshot saved. Recorded files: " << files.size() << std::endl;
 }
 
+std::unordered_map<std::string, FileMeta> load_snapshot(const std::string& db_path) {
+    //функция для выгрузки данных из сохраненного ранее снапшота
+    std::unordered_map<std::string, FileMeta> past_snapshot;
+    std::ifstream in(db_path);
+    if(!in.is_open()) {
+        std::cerr << "Error: no such snaphot file on " << db_path << std::endl;
+        return past_snapshot; //в случае если открыть файл снапшота не удалось выводим ошибку
+    }
+    std::string line;
+    while (std::getline(in,line)) {//пройдемся по всем строкам файла
+        if (line.empty()) {
+            continue;//пропустим пустые строки
+        }
+        std::stringstream ss(line);//работать будем со строковым потоком
+        std::string path_part, size_part, time_part;
+        if (std::getline(ss, path_part, '|') && std::getline(ss, size_part, '|') && std::getline(ss, time_part, '|')) {//разобьем строку с метаданными на отдельные части метаданных файла
+            FileMeta meta;
+            meta.path = path_part;
+            meta.size = std::stoll(size_part);
+            meta.modification_time = std::stoll(time_part);
+            past_snapshot[meta.path] = meta;//заполним хеш-таблицу набором метаданных файла по пути в качестве ключа
+        }
+    }
+    in.close();
+    std::cout << "succesfully Loaded files from snaphot. count: " << past_snapshot.size() << std::endl; //выве
+    return past_snapshot; // вернем набор метаданных файлов
+}
+
+void compare_snapshots(const std::vector<FileMeta>& current, std::unordered_map<std::string,FileMeta>& past) {
+    //Функция для сравнения текущего состояние директории со считанным состоянием из снапшота 
+    std::cout << "Compare" << std::endl;
+    int new_count = 0, modified_count = 0,deleted_count = 0;
+    for (const auto& current_file : current) {//пройдемся по всем файлам
+        auto past_file = past.find(current_file.path);
+        if (past_file == past.end()) {//если файнд дошел до конца таблицы, значит ранее этого файла не было и => он новый 
+            std::cout << "new file on " << current_file.path << std::endl;
+            new_count++;
+        }
+        else {//иначе файл раньше был. если размер или время изменения разные, значит файл изменен
+            if (current_file.size != past_file->second.size || current_file.modification_time != past_file->second.modification_time) {
+                std::cout << "modified file on " << current_file.path << std::endl;
+                modified_count++;
+            }
+            past.erase(past_file);//удалим файл из хеш-таблицы, чтобы в ней после последней итерации фора остались те файлы, которые в новом состоянии найти не получится
+        }
+        
+    }
+    for (const auto& pair : past) {//сейчас в хеш-таблице остались файлы, которых нет в текущем состоянии и => эти файлы были удалены
+        std::cout << "deleted file on " << pair.first << std::endl;
+        deleted_count++;
+    }
+    std::cout << "Summary: New: " << new_count << ", modified: " << modified_count << ", deleted: " << deleted_count << std::endl;//выведем сводку по сравнению директории со снапшотом
+}
+
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    /*std::cout << "тест снапшота" << std::endl;
+    std::string target = "/app";
+    std::string database_file = "/app/snapshot.db";
+    std::cout << "scanning" << std::endl;
+    std::vector<FileMeta> current_files = scan_directory(target);
+    if (current_files.empty()) {
+        std::cout << "directory is empty" << std::endl;
+        return 1;
+    }
+    std::cout << "scanned" << std::endl;
+    save_snapshot(database_file, current_files);
+    std::cout << "Ready" << std::endl;
+    return 0;*/
+    std::string target_dir = "/app";
+    std::string database_file = "/app/snapshot.db";
+
+    std::cout << "тест сравнения" << std::endl;
+    std::unordered_map<std::string, FileMeta> past_snapshot = load_snapshot(database_file);
+
+    std::cout << "\nScanning directory for current state..." << std::endl;
+    std::vector<FileMeta> current_state = scan_directory(target_dir);
+
+    compare_snapshots(current_state, past_snapshot);
+
+    std::cout << "финишед" << std::endl;
     return 0;
 }
